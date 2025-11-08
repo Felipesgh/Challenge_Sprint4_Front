fb90eea9b16435120af6bdef4cd325b97bda4986
Felipe Garcia <lippe-sg@hotmail.com>
2025-11-08 09:49:39 -0300

    Atualização do Projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-
 import React from "react"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Home from "./pages/Home"
@@ -9,6 +8,8 @@ import IntegranteDetail from "./pages/IntegrantesDetail"
 import Faq from "./pages/Faq"
 import Contato from "./pages/Contato"
 import Solucao from "./pages/Solucao"
+import NotFound from "./pages/NotFound"
+import BuscarCep from "./pages/BuscarCep"
 
 
 export default function App(){
@@ -23,6 +24,9 @@ export default function App(){
           <Route path="/faq" element={<Faq />} />
           <Route path="/contato" element={<Contato />} />
           <Route path="/solucao" element={<Solucao />} />
+          <Route path="/home" element={<Navigate to="/" replace />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/buscar-cep" element={<BuscarCep />} />
         </Routes>
       </main>
       <Footer />
